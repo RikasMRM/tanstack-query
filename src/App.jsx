286db477b4f912fc3c9ec17af0f8e74b23ce3870
Rikas <rikasrkf@gmail.com>
2023-07-24 12:35:24 +0530

    Basic example

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const queryClient = useQueryClient();
+
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: (title) => {
+      wait(1000).then(() => POSTS.push({ id: crypto.randomUUID, title }));
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {postsQuery.data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+
+      <button
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate("New Post")}
+      >
+        Add New
+      </button>
+    </div>
+  );
 }
 
-export default App
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+export default App;
